8b4d4f06f3accb25d3cda6d8044e3222129db994
gabriel-demes <Gabrieldemes@nyu.edu>
2021-02-22 12:51:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = ""
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "bob"
+
+}
