57e2592af0f06480bbd1cbb78ebc1be0ea9d2e11
kendofriendo <goadke@g.cofc.edu>
2021-04-08 07:31:03 -0400

    finish

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = 'boba'
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bobb';
+}
